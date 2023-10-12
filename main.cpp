e75b54b60008a7b4e267c224744a23e31a1cd0ec
Andrew_Yankee <144984404+CS230350@users.noreply.github.com>
2023-10-12 11:35:41 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,105 @@
+//
+//  main.cpp
+//  apple
+//
+//  Created by Dmitriy Taldonov on 11/10/23.
+//
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int main (){
+//
+//    int age = 1;
+//    while ( age <= 71 )
+//    {
+//        age++;
+//
+//        if (age <= 18){
+//            cout << "School time: " << age << endl;
+//        }
+//        else if (age >= 18 && age <= 22){
+//        cout << "University time: " << age << endl;
+//        }
+//
+//        else if (age >= 22 && age<= 50){
+//        cout << "Job time: " << age << endl;}
+//
+//        else if (age >= 50 && age<= 70){
+//        cout << "retired time: " << age << endl;}
+//
+//        }
+//    return 0;
+//    }
+//
+
+
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int main (){
+//
+//    int age = 0;
+//    cout << "write an age: " << endl;
+//    cin >> age;
+//    {
+//
+//
+//        if (age <= 18){
+//            cout << "School time: " << age << endl;
+//        }
+//        else if (age >= 18 && age <= 22){
+//        cout << "University time: " << age << endl;
+//        }
+//
+//        else if (age >= 22 && age<= 50){
+//        cout << "Job time: " << age << endl;}
+//
+//        else if (age >= 50 && age<= 70){
+//        cout << "retired time: " << age << endl;}
+//
+//        }
+//    return 0;
+//    }
+
+    
+
+
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int main (){
+//
+//    int i = 100;
+//    do
+//    {
+//    cout << i << "\r";
+//    i = i - 10 ;
+//    }
+//    while ( i >= 10);
+//
+//return 0;
+//}
+
+
+
+
+//
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int main (){
+//    int i;
+//    int j;
+//    
+//    for (i = 0; i < 10; i++) {
+//        for (j = 0; j < i; j++) {
+//            printf("*\n", &j[i]);
+//        }
+//        printf("\n");
+//    }
+//    
+//    return 0;
+//}
